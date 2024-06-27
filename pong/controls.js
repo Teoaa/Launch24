e87b6f0905b3981b15ad0a1aa22dabb119b142fc
TeomanYilmaz <tyilmaz@wpi.edu>
2024-06-27 15:00:18 -0400

    Fix change issue

--- pong/controls.js
+++ pong/controls.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const UPARROW = 38;
 const DOWNARROW = 40;
 const WKEY = 87;
@@ -42,49 +41,4 @@ function keyUp(event) {
         default:
     }
 }
-=======
-const UPARROW = 38;
-const DOWNARROW = 40;
-const WKEY = 87;
-const SKEY = 83;
-
-let paddleVelocity = 5;
-
-function keyDown(event) {
-    const key = event.keyCode;
-    //console.log(`KEYDOWN: ${key}`);
-    switch (key) {
-        case (UPARROW):
-            paddleR.vy = -paddleVelocity;
-            break;
-        case (DOWNARROW):
-            paddleR.vy = paddleVelocity;
-            break;
-        case (WKEY):
-            paddleL.vy = -paddleVelocity;
-            break;
-        case (SKEY):
-            paddleL.vy = paddleVelocity;
-            break;
-        default:
-    }
-}
-window.addEventListener("keydown", keyDown);
-
-function keyUp(event) {
-    const key = event.keyCode;
-    //console.log(`KEYUP: ${key}`);
-    switch (key) {
-        case (UPARROW):
-        case (DOWNARROW):
-            paddleR.vy = 0;
-            break;
-        case (WKEY):
-        case (SKEY):
-            paddleL.vy = 0;
-            break;
-        default:
-    }
-}
->>>>>>> 9f261fa9a9f80d9ded57c6c0d0a076ddb7aaf1de
 window.addEventListener("keyup", keyUp);
